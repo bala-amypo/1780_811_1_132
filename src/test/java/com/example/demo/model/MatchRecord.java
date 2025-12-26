1251956267ea228255a731275352bf927d906bd4
bala-amypo <bala@amypo.in>
2025-12-26 03:55:30 +0000

    User commit

---
+++ src/test/java/com/example/demo/model/MatchRecord.java
@@ -0,0 +1,80 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "match_records")
+public class MatchRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String status = "PENDING";
+
+    @ManyToOne
+    @JoinColumn(name = "user_a_id")
+    private UserProfile userA;
+
+    @ManyToOne
+    @JoinColumn(name = "user_b_id")
+    private UserProfile userB;
+
+    @ManyToOne
+    @JoinColumn(name = "skill_a_id")
+    private Skill skillOfferedByA;
+
+    @ManyToOne
+    @JoinColumn(name = "skill_b_id")
+    private Skill skillOfferedByB;
+
+    // ===== Getters & Setters =====
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public UserProfile getUserA() {
+        return userA;
+    }
+
+    public void setUserA(UserProfile userA) {
+        this.userA = userA;
+    }
+
+    public UserProfile getUserB() {
+        return userB;
+    }
+
+    public void setUserB(UserProfile userB) {
+        this.userB = userB;
+    }
+
+    public Skill getSkillOfferedByA() {
+        return skillOfferedByA;
+    }
+
+    public void setSkillOfferedByA(Skill skillOfferedByA) {
+        this.skillOfferedByA = skillOfferedByA;
+    }
+
+    public Skill getSkillOfferedByB() {
+        return skillOfferedByB;
+    }
+
+    public void setSkillOfferedByB(Skill skillOfferedByB) {
+        this.skillOfferedByB = skillOfferedByB;
+    }
+}
